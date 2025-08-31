862c9f0b00ebe3b4abe59692a8e671fee5ec6ecf
frixo <frixo3190@gmail.com>
2025-08-31 23:46:33 +0200

    m

--- components/acw02/__init__.py
+++ components/acw02/__init__.py
@@ -1,20 +1,16 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import uart
+from esphome.components import binary_sensor
 from esphome.const import CONF_ID
 
-CONF_UART_ID = "uart_id"
+empty_binary_sensor_ns = cg.esphome_ns.namespace('acw02')
+empty_binary_sensor_ns = empty_binary_sensor_ns.class_('EmptyBinarySensor', binary_sensor.BinarySensor, cg.Component)
 
-acw02_ns = cg.esphome_ns.namespace("acw02")
-ACW02 = acw02_ns.class_("ACW02", cg.Component, uart.UARTDevice)
-
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(ACW02),
-    cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
-})
+CONFIG_SCHEMA = binary_sensor.BINARY_SENSOR_SCHEMA.extend({
+    cv.GenerateID(): cv.declare_id(empty_binary_sensor_ns)
+}).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    uart_var = await cg.get_variable(config[CONF_UART_ID])
-    cg.add(var.set_uart_parent(uart_var))
+    await binary_sensor.register_binary_sensor(var, config)
