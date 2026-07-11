c3bdfbe8285fde8968c5dc09c9d6619940bb22d3
frixo <frixo3190@gmail.com>
2026-07-11 13:23:13 +0200

    m

--- tuya_patched/tuya/tuya.h
+++ tuya_patched/tuya/tuya.h
@@ -86,9 +86,6 @@ struct TuyaCommand {
 
 class Tuya : public Component, public uart::UARTDevice {
  public:
-  void query_all_datapoints() {
-    this->send_empty_command_(TuyaCommandType::DATAPOINT_QUERY);
-  }
   float get_setup_priority() const override { return setup_priority::LATE; }
   void setup() override;
   void loop() override;
